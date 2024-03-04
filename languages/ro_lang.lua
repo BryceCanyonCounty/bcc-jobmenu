6735a7b0956d5245f062e274ffaa61b35fea787b
iseeyoucopy <iseeyoucopy@yahoo.com>
2024-03-04 13:11:32 +0000

    Create ro_lang.lua
    Added Romanian language support

---
+++ languages/ro_lang.lua
@@ -0,0 +1,14 @@
+Locales["ro_lang"] = {
+    MenuTitle         = "Meniu Job",
+    MenuSubTitle      = "Selectati un job",
+    GradeMenuTitle    = "Meniu Grad",
+    GradeMenuSubTitle = "Selectati un grad",
+    JobGrade          = "Grad Job",
+    confirm           = "Confirmare",
+    empty             = "Trebuie sa introduceti un grad!",
+    jobgiven          = "Job-ul tau este acum: ~o~",
+    gradegiven        = "Cu gradul de: ~o~",
+    JobChangeDelay    = "Poti schimba jobul in: ~o~",
+    TimeFormat        = " minute",
+    Back              = "Inapoi",
+}
