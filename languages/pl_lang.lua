0912fb9a1073591cb03ea1a37320d56920bbd579
sajter <67130554+sajter@users.noreply.github.com>
2025-12-21 16:31:31 +0100

    Create pl_lang.lua

---
+++ languages/pl_lang.lua
@@ -0,0 +1,14 @@
+Locales["pl_lang"] = {
+    MenuTitle         = "Menu Pracy",
+    MenuSubTitle      = "Wybierz Pracę",
+    GradeMenuTitle    = "Menu Stopni",
+    GradeMenuSubTitle = "Wybierz Stopień",
+    JobGrade          = "Stopień Zawodowy",
+    confirm           = "Potwierdź",
+    empty             = "Musisz wprowadzić stopień!",
+    jobgiven          = "Twoja praca to teraz: ~o~",
+    gradegiven        = "Ze stopniem: ~o~",
+    JobChangeDelay    = "Zmiana pracy za: ~o~",
+    TimeFormat        = " min.",
+    Back              = "Wstecz",
+}
