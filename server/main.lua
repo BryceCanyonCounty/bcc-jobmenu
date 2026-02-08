0d2d3fe52da7e30f70138876fe95fd77ad7b1058
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-02-08 00:56:07 -0800

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,15 +1,21 @@
-local VORPcore = exports.vorp_core:GetCore()
-
-VORPcore.Callback.Register('bcc:set_job', function(source, cb, data)
+Core.Callback.Register('bcc:set_job', function(source, cb, data)
     local src = source
-    local character = VORPcore.getUser(src).getUsedCharacter
+    local user = Core.getUser(src)
+
+    -- Check if the user exists
+    if not user then
+        DBG:Error('User not found for source: ' .. tostring(src))
+        return cb(false)
+    end
+
+    local character = user.getUsedCharacter
 
     character.setJob(data.job)
     character.setJobGrade(data.grade)
     character.setJobLabel(data.label)
 
-    VORPcore.NotifyRightTip(src, _U('jobgiven') .. data.label, 4000)
-    VORPcore.NotifyRightTip(src, _U('gradegiven') .. data.grade .. " " .. "( " .. data.grade_label .. " )", 4000)
+    Core.NotifyRightTip(src, _U('jobgiven') .. data.label, 4000)
+    Core.NotifyRightTip(src, _U('gradegiven') .. data.grade .. " " .. "( " .. data.grade_label .. " )", 4000)
 
     cb(true)
 end)
