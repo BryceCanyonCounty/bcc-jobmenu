61e2b56bd766b142bfde3d0a4ee21f1d344ad80c
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-02-08 00:55:43 -0800

    Create jobs.lua

---
+++ configs/jobs.lua
@@ -0,0 +1,67 @@
+Jobs = {
+    {
+        label = "Police",
+        value = "police",
+        jobGrades = {
+            {
+                label = "Cadet",
+                grade = 1,
+
+            },
+            {
+                label = "Officer",
+                grade = 2,
+
+            },
+            {
+                label = "Sergeant",
+                grade = 3,
+
+            },
+            {
+                label = "Lieutenant",
+                grade = 4,
+
+            },
+            {
+                label = "Captain",
+                grade = 5,
+
+            },
+            {
+                label = "Chief",
+                grade = 6,
+
+            },
+        }
+    },
+    -----------------------------------------------------
+
+    {
+        label = "Doctor",
+        value = "doctor",
+        jobGrades = false
+    },
+    -----------------------------------------------------
+
+    {
+        label = "Train Driver",
+        value = "trainer",
+        jobGrades = true
+    },
+    -----------------------------------------------------
+
+    {
+        label = "Farmer",
+        value = "farmer",
+        jobGrades = true
+    },
+    -----------------------------------------------------
+
+    {
+        label = "Rancher",
+        value = "rancher",
+        jobGrades = false
+    },
+    -----------------------------------------------------
+}
