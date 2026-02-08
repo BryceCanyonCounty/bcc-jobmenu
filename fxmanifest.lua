99113f86cff2e9a63642c44feedfdfa22fc53cf9
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-02-08 00:55:51 -0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,27 +1,31 @@
-fx_version 'adamant'
-games { 'rdr3' }
+fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
+game 'rdr3'
+lua54 'yes'
 author 'SavSin'
 description 'Simple Job Menu'
 
-ui_page 'html/index.html'
-
 client_scripts {
-    'client/*.lua',
+    'client/client_init.lua',
+    'client/menu.lua',
+    'client/main.lua'
 }
 
-shared_script {
-    'config.lua',
+shared_scripts {
+    'configs/*.lua',
     'locale.lua',
-    'languages/*.lua',
+    'languages/*.lua'
 }
 
 server_scripts {
-    'server/*.lua',
+    'server/server_init.lua',
+    'server/main.lua'
 }
 
 dependencies {
     'vorp_core',
     'feather-menu'
 }
+
+version '2.1.0'
